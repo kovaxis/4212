0a4b72ceaae5e5f36f15ae81df4d50a4768c9785
negamartin <martin.andrighetti@gmail.com>
2020-12-09 03:50:24 -0300

    Improve wordlist by hand-picking words
    Guess passphrases are now 43-bit. Oops.

--- make_wordlist.py
+++ make_wordlist.py
@@ -1,14 +1,16 @@
 
 import sys
 
-if len(sys.argv) < 1 + 4:
+if len(sys.argv) < 1 + 5:
     raise Exception(
-        "usage: py make_wordlist.py <input> <min length> <prefix length> <prefer function>")
+        "usage: py make_wordlist.py <input> <output> <min length> <prefix length> <prefer function> [choice file]")
 
-inpath = sys.argv[1]
-minlen = int(sys.argv[2])
-prefixlen = int(sys.argv[3])
-prefname = sys.argv[4]
+[_, inpath, outpath, minlen, prefixlen, prefname] = sys.argv[:6]
+minlen = int(minlen)
+prefixlen = int(prefixlen)
+choicepath = None
+if len(sys.argv) > 6:
+    choicepath = sys.argv[6]
 
 # Prefer function
 prefer = ({
@@ -16,6 +18,7 @@ prefer = ({
     'longest': lambda old, new: len(new) > len(old),
     'first': lambda old, new: False,
     'last': lambda old, new: True,
+    'manual': None,
 })[prefname]
 
 # Group words by their prefix
@@ -26,28 +29,74 @@ for word in file.readlines():
     by_prefix.setdefault(word[:prefixlen], []).append(word)
 file.close()
 
+# Load saved choices
+saved_choices = {}
+if choicepath:
+    try:
+        file = open(choicepath, "r")
+        for line in file.readlines():
+            [pre, word] = line.split('=')
+            saved_choices[pre.strip()] = word.strip()
+        file.close()
+    except IOError:
+        print("failed to open savefile")
 
 # Resolve prefix conflicts through `prefer`
 wordlist = []
+choicefile = None
+if choicepath != None:
+    choicefile = open(choicepath, "a")
 for prefix, words in by_prefix.items():
+    if all(map(lambda w: len(w) < minlen, words)):
+        continue
     preferred = None
-    any_long_enough = False
-    for word in words:
-        if preferred == None or prefer(preferred, word):
-            preferred = word
-        if len(word) >= minlen:
-            any_long_enough = True
-    if any_long_enough:
+    if prefer == None:
+        saved = saved_choices.get(prefix, None)
+        if saved != None:
+            if saved != "":
+                wordlist.append(saved)
+            continue
+        i = 0
+        while i < len(words):
+            w = words[i]
+            for j in range(len(words)-1, i, -1):
+                if w == words[j]:
+                    del words[j]
+            i += 1
+        words.sort(key=lambda w: len(w))
+        print()
+        for i in range(len(words)):
+            print(f"{i+1}. {words[i]}")
+        num = None
+        while num == None or num < 0 or num > len(words):
+            try:
+                num = int(input())
+            except ValueError:
+                pass
+        if num > 0:
+            preferred = words[num - 1]
+        if choicefile:
+            choicefile.write(f"{prefix} = {preferred or ''}\n")
+            choicefile.flush()
+    else:
+        for word in words:
+            if preferred == None or prefer(preferred, word):
+                preferred = word
+    if preferred != None:
         wordlist.append(preferred)
+if choicefile:
+    choicefile.close()
 
-# Print entire wordlist as a space-separated string
-print(f"{len(wordlist)} words:")
-print('"', end="")
+# Write entire wordlist as a space-separated string
+file = open(outpath, "a")
+file.write(f"{len(wordlist)} words:\n")
+file.write('"')
 first = True
 for word in wordlist:
     if first:
         first = False
     else:
-        print(" ", end="")
-    print(word, end="")
-print('"')
+        file.write(" ")
+    file.write(word)
+file.write('"\n')
+file.close()
